111ed3efc6445c3ec11188096a22946a7adb57a3
Mysh3ll <michel.pompas@greta-sud-aquitaine.fr>
2016-06-01 21:16:15 +0200

    Correction undefined variable $validation

--- miseAJour.php
+++ miseAJour.php
@@ -132,8 +132,10 @@ if (Input::existe()) {
                     <strong>Erreur!</strong> </br>
                     <?php
                     if ($erreurSaisie) {
-                        foreach ($validation->erreurs() as $erreurSaisie) {
-                            echo $erreurSaisie . '</br>';
+                        if (isset($validation)) {
+                            foreach ($validation->erreurs() as $erreurSaisie) {
+                                echo $erreurSaisie . '</br>';
+                            }
                         }
                     }
                     if ($erreurInsertion) {
